81ea5d225ca514d8d39f99225b455a0a632dc515
Claudio <claudio.marino@outlook.com>
2025-07-13 18:41:34 +0200

    GENERAL STYLES OF SETTINGS SCREEN

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -3,10 +3,19 @@ import { StyleSheet, Text, View } from 'react-native'
 
 export default function SettingsScreen() {
 	return (
-		<View>
-			<Text>SettingsScreen</Text>
+		<View style={styles.container}>
+			<Text style={styles.text}>SettingsScreen</Text>
 		</View>
 	)
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	text: {
+		fontSize: 22,
+	},
+})
