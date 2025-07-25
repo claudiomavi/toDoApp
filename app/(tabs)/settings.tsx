673066e495c3f3cc9cf430068074360a305be70e
Claudio <claudio.marino@outlook.com>
2025-07-25 20:37:33 +0200

    COMPONETIZED THE PAGE AND REMOVED THE STATS PART

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,10 +1,9 @@
 import { createSettingsStyles } from '@/assets/styles/settings.styles'
-import ProgressStats from '@/components/ProgressStats/ProgressStats'
+import GeneralHeader from '@/components/GeneralHeader'
 import useTheme from '@/hooks/useTheme'
-import { Ionicons } from '@expo/vector-icons'
 import { LinearGradient } from 'expo-linear-gradient'
 import { useState } from 'react'
-import { ScrollView, Text, View } from 'react-native'
+import { ScrollView } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function SettingsScreen() {
@@ -21,30 +20,16 @@ export default function SettingsScreen() {
 			style={settingsStyles.container}
 		>
 			<SafeAreaView style={settingsStyles.safeArea}>
-				{/* HEADER */}
-				<View style={settingsStyles.header}>
-					<View style={settingsStyles.titleContainer}>
-						<LinearGradient
-							colors={colors.gradients.primary}
-							style={settingsStyles.iconContainer}
-						>
-							<Ionicons
-								name="settings"
-								size={28}
-								color="#fff"
-							/>
-						</LinearGradient>
-						<Text style={settingsStyles.title}>Settings</Text>
-					</View>
-				</View>
+				<GeneralHeader
+					title="Settings"
+					icon="settings"
+				/>
 
 				<ScrollView
 					style={settingsStyles.scrollView}
 					contentContainerStyle={settingsStyles.content}
 					showsVerticalScrollIndicator={false}
-				>
-					<ProgressStats />
-				</ScrollView>
+				></ScrollView>
 			</SafeAreaView>
 		</LinearGradient>
 	)
